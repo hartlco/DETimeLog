7343a0a90f47f739fc10aeb41c07d18ceb177903
Martin Hartl <martin@hartl.co>
2022-02-15 08:23:13 +0100

    Initial Commit

---
+++ Shared/DETimeLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  DETimeLogApp.swift
+//  Shared
+//
+//  Created by martinhartl on 15.02.22.
+//
+
+import SwiftUI
+
+@main
+struct DETimeLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
